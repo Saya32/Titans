c08959615af3240925ceabbfa5de4cd280c16b17
Janet <k21003182@kcl.ac.uk>
2023-01-27 17:36:58 +0000

    Add testing to Onboarding

---
+++ spendingtrackers/tests/views/test_home_view.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class HomeViewTestCase(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_search_by_name(self):
+        response = self.client.get(reverse("home_page"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("home_page"))
+        self.assertTemplateUsed(response, "home_page.html")from django.test import TestCase
+from django.urls import reverse
+
+class HomeViewTestCase(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_search_by_name(self):
+        response = self.client.get(reverse("home_page"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("home_page"))
+        self.assertTemplateUsed(response, "home_page.html")
