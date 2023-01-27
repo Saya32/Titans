3d1b6950faf00781cbc0d4c3fb6c0fff5a3db9c4
Sayaka Bhandari <sayakabhandari@gmail.com>
2023-01-27 16:35:36 +0000

    Implement onboarding pages

---
+++ spendingtrackers/forms.py
@@ -0,0 +1,71 @@
+from django import forms
+from .models import User
+from django.utils import timezone
+from django.core.validators import RegexValidator
+from django.contrib.auth import authenticate
+from django.db import models
+from django.contrib.auth.forms import UserChangeForm
+
+class LogInForm(forms.Form):
+    """Form enabling registered users to log in."""
+
+    username = forms.CharField(label="Email")
+    password = forms.CharField(label="Password", widget=forms.PasswordInput())
+
+    def get_user(self):
+        """Returns authenticated user if possible."""
+
+        user = None
+        if self.is_valid():
+            username = self.cleaned_data.get('username')
+            password = self.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+        return user
+
+class SignUpForm(forms.ModelForm):
+    """Form enabling unregistered users to sign up."""
+
+    class Meta:
+        """Form options."""
+
+        model = User
+        fields = ['first_name', 'last_name', 'username']
+        labels = {
+            'username': 'Email',
+        }
+
+
+    new_password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(),
+        validators=[RegexValidator(
+            regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).*$',
+            message='Password must contain an uppercase character, a lowercase '
+                    'character and a number'
+            )]
+    )
+    password_confirmation = forms.CharField(label='Password confirmation', widget=forms.PasswordInput())
+
+    def clean(self):
+        """Clean the data and generate messages for any errors."""
+
+        super().clean()
+        new_password = self.cleaned_data.get('new_password')
+        password_confirmation = self.cleaned_data.get('password_confirmation')
+        if new_password != password_confirmation:
+            self.add_error('password_confirmation', 'Confirmation does not match password.')
+
+    def save(self):
+        """Create a new user."""
+
+        super().save(commit=False)
+        user = User.objects.create_user(
+            self.cleaned_data.get('username'),
+            first_name=self.cleaned_data.get('first_name'),
+            last_name=self.cleaned_data.get('last_name'),
+            password=self.cleaned_data.get('new_password'),
+            is_student = True,
+
+
+        )
+        return user
--- spendingtrackers/views.py
+++ spendingtrackers/views.py
@@ -1,3 +1,91 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, redirect
+from .models import User
+from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm, LogInForm
+from django.contrib.auth import login, logout
+##from .decorators import student_required, director_required, admin_required
+from django.contrib import messages
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.views import View
+from django.views.generic.edit import FormView
+from django.urls import reverse
+from django.core.exceptions import PermissionDenied
+
+class LoginProhibitedMixin:
+    """Mixin that redirects when a user is logged in."""
+
+    redirect_when_logged_in_url = None
+
+    def dispatch(self, *args, **kwargs):
+        """Redirect when logged in, or dispatch as normal otherwise."""
+        if self.request.user.is_authenticated:
+            return self.handle_already_logged_in(*args, **kwargs)
+        return super().dispatch(*args, **kwargs)
+
+    def handle_already_logged_in(self, *args, **kwargs):
+        url = self.get_redirect_when_logged_in_url()
+        return redirect(url)
+
+    def get_redirect_when_logged_in_url(self):
+        """Returns the url to redirect to when not logged in."""
+        if self.redirect_when_logged_in_url is None:
+            raise ImproperlyConfigured(
+                "LoginProhibitedMixin requires either a value for "
+                "'redirect_when_logged_in_url', or an implementation for "
+                "'get_redirect_when_logged_in_url()'."
+            )
+        else:
+            return self.redirect_when_logged_in_url
+
+
+class LogInView(LoginProhibitedMixin, View):
+    """View that handles log in."""
+
+    http_method_names = ['get', 'post']
+    redirect_when_logged_in_url = 'feed'
+
+    def get(self, request):
+        """Display log in template."""
+
+        self.next = request.GET.get('next') or ''
+        return self.render()
+
+    def post(self, request):
+        """Handle log in attempt."""
+
+        form = LogInForm(request.POST)
+        self.next = request.POST.get('next') or settings.REDIRECT_URL_WHEN_LOGGED_IN
+        user = form.get_user()
+        if user is not None:
+            login(request, user)
+            return redirect(self.next)
+
+        messages.add_message(request, messages.ERROR, "The credentials provided were invalid!")
+        return self.render()
+
+    def render(self):
+        """Render log in template with blank log in form."""
+
+        form = LogInForm()
+        return render(self.request, 'log_in.html', {'form': form, 'next': self.next})
+
+
+class SignUpView(LoginProhibitedMixin, FormView):
+    """View that signs up user."""
+
+    form_class = SignUpForm
+    template_name = "sign_up.html"
+    redirect_when_logged_in_url = settings.REDIRECT_URL_WHEN_LOGGED_IN
+
+    def form_valid(self, form):
+        self.object = form.save()
+        login(self.request, self.object)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse(settings.REDIRECT_URL_WHEN_LOGGED_IN)
+    
+def home_page(request):
+    return render(request, 'home_page.html')
