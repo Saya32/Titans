02f48053186419f67d26aa14eba496e33e28c868
Janet <k21003182@kcl.ac.uk>
2023-02-24 16:54:18 +0000

    Add tests for category detials form

--- spendingtrackers/tests/forms/test_category_details_form.py
+++ spendingtrackers/tests/forms/test_category_details_form.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.test import TestCase
-from ...forms import TransactionForm
+from ...forms import CategoryDetailsForm
 from ...models import Transaction, User
 from django.utils import timezone
 from django.core.files.uploadedfile import SimpleUploadedFile
@@ -17,58 +17,48 @@ class CategoryDetailsFormTestCase(TestCase):
         self.form_input = {
             'name':'New',
             'budget':10,
-            
+            'start_date':'2022-12-12',
+            'end_date': '2023-12-12'
         }
 
     def test_valid_transaction_form(self):
-        form = TransactionForm(data=self.form_input)
+        form = CategoryDetailsForm(data=self.form_input)
         self.assertTrue(form.is_valid())
 
     def test_form_has_necessary_fields(self):
-        form = TransactionForm()
-        self.assertIn('title', form.fields)
+        form = CategoryDetailsForm()
+        self.assertIn('name', form.fields)
 
-        self.assertIn('description', form.fields)
         
-        self.assertIn('amount', form.fields)
-        amount = form.fields['amount']
-        self.assertTrue(isinstance(amount, forms.IntegerField))
-
-        self.assertIn('date_paid', form.fields)
-        date_paid = form.fields['date_paid']
-        self.assertTrue(isinstance(date_paid, forms.DateField))
-
-        self.assertIn('time_paid', form.fields)
-        time_paid = form.fields['time_paid']
-        self.assertTrue(isinstance(time_paid, forms.TimeField))
-
-        self.assertIn('category', form.fields)
-        category = form.fields['category']
-        self.assertTrue(isinstance(category, forms.ChoiceField))
-
-        self.assertIn('receipt', form.fields)
-        receipt = form.fields['receipt']
-        self.assertTrue(isinstance(receipt, forms.ImageField))
         
-        self.assertIn('transaction_type', form.fields)
-        transaction_type = form.fields['transaction_type']
-        self.assertTrue(isinstance(transaction_type, forms.ChoiceField))
+        self.assertIn('budget', form.fields)
+        budget = form.fields['budget']
+        self.assertTrue(isinstance(budget, forms.IntegerField))
 
+        self.assertIn('start_date', form.fields)
+        start_date = form.fields['start_date']
+        self.assertTrue(isinstance(start_date, forms.DateField))
+
+        self.assertIn('end_date', form.fields)
+        end_date = form.fields['end_date']
+        self.assertTrue(isinstance(end_date, forms.DateField))
+
+    
 
 
     def test_form_uses_model_validation(self):
-        self.form_input['amount'] = 61.2222
-        form = TransactionForm(data=self.form_input)
+        self.form_input['budget'] = 61.2222
+        form = CategoryDetailsForm(data=self.form_input)
         self.assertFalse(form.is_valid())
     
-    def test_date_paid_should_not_be_none(self):
+    def test_date_paid_should_can_be_none(self):
         self.form_input['date_paid'] = ""
-        form = TransactionForm(data=self.form_input)
-        self.assertFalse(form.is_valid())
+        form = CategoryDetailsForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
     
-    def test_time_paid_should_not_be_none(self):
+    def test_time_paid_can_be_none(self):
         self.form_input['time_paid'] = ""
-        form = TransactionForm(data=self.form_input)
-        self.assertFalse(form.is_valid())
+        form = CategoryDetailsForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
     
 
