49ff359368f02c740e04d68e173d175a33d5a60e
Aayush Dwivedi <K21077065@kcl.ac.uk>
2023-03-17 10:40:38 +0000

    changes to test test_dashboard

--- spendingtrackers/tests/views/test_dashboard.py
+++ spendingtrackers/tests/views/test_dashboard.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from spendingtrackers.forms import TransactionForm
 from spendingtrackers.models import User, Transaction
-from spendingtrackers.tests.helpers import reverse_with_next, create_transactions
+from spendingtrackers.tests.helpers import reverse_with_next, create_transactions, create_categories
 
 class DashboardTestCase(TestCase):
     """Test case of Dashboard view"""
@@ -16,31 +16,8 @@ class DashboardTestCase(TestCase):
         self.url = reverse('dashboard')
         self.user = User.objects.get(username='johndoe@example.org')
         create_transactions(self.user,0,2)
+        create_categories(self.user,0,2)
         self.transactions = Transaction.objects.filter()
-        # Create a user
-        self.user = User.objects.create_user(
-            username='testuser',
-            password='password123'
-        )
-        # Create some expense transactions for the user
-        Transaction.objects.create(
-            user=self.user,
-            transaction_type='Expense',
-            category='Food',
-            amount=20
-        )
-        Transaction.objects.create(
-            user=self.user,
-            transaction_type='Expense',
-            category='Transportation',
-            amount=10
-        )
-        Transaction.objects.create(
-            user=self.user,
-            transaction_type='Expense',
-            category='Food',
-            amount=15
-        )
 
     def test_dashboard_url(self):
         self.assertEqual(self.url,'/dashboard/')
@@ -53,22 +30,30 @@ class DashboardTestCase(TestCase):
 
     def test_chart_balance_graph(self):
         # Create some sample transactions
-        user = User.objects.create_user(username='testuser', password='testpass')
-        transaction1 = Transaction.objects.create(user=user, transaction_type='Income', amount=100, date_paid=datetime.date(2022, 1, 1))
-        transaction2 = Transaction.objects.create(user=user, transaction_type='Expense', amount=50, date_paid=datetime.date(2022, 1, 2))
-        transaction3 = Transaction.objects.create(user=user, transaction_type='Expense', amount=25, date_paid=datetime.date(2022, 1, 3))
-        # Make a request to the view
-        request = HttpRequest()
-        request.user = user
-        response = chart_balance_graph(request)
-        data = json.loads(response.content.decode('utf-8'))
+        self.client.login(username=self.user.username, password='Password123')
+        create_transactions(self.user, 0, 2)
+        transaction1 = Transaction.objects.first()
+        transaction2 = Transaction.objects.last()
+        transaction1.date_paid = '01/01/2022'
+        transaction2.date_paid = '01/02/202'
+        transaction1.transaction_type = 'Income'
+        transaction2.transaction_type = 'Expense'
+        transaction1.amount = '100'
+        transaction2.amount = '50'
+        transaction1.save()
+        transaction2.save()
+        
+        response = self.client.post(self.url, {'from_date': '2023-02-14', 'to_date': '2023-02-16'})
         # Check that the labels and data are correct
-        self.assertEqual(data['labels'], ['01/01/2022', '01/02/2022', '01/03/2022'])
-        self.assertEqual(data['data'], [100, 50, 25])
+        self.assertEqual(data['labels'], ['01/01/2022', '01/02/2022'])
+        self.assertEqual(data['data'], [100, 50])
+        self.assertTemplateUsed(response, 'dashboard.html')
+        self.assertIn(transaction1, response.context['transactions'])
+        self.assertNotIn(transaction2, response.context['transactions'])
 
-    def test_chart_expense_graph(self):
+    #def test_chart_expense_graph(self):
         # Create some sample transactions
-        user = User.objects.create_user(username='testuser', password='testpass')
+        self.client.login(username=self.user.username, password='Password123')
         transaction1 = Transaction.objects.create(user=user, transaction_type='Income', amount=100, date_paid=datetime.date(2022, 1, 1))
         transaction2 = Transaction.objects.create(user=user, transaction_type='Expense', amount=50, date_paid=datetime.date(2022, 1, 2))
         transaction3 = Transaction.objects.create(user=user, transaction_type='Expense', amount=25, date_paid=datetime.date(2022, 1, 3))
@@ -81,9 +66,9 @@ class DashboardTestCase(TestCase):
         self.assertEqual(data['labels'], ['01/01/2022', '01/02/2022', '01/03/2022'])
         self.assertEqual(data['data'], [0, 50, 25])
 
-    def test_chart_income_graph(self):
+    #def test_chart_income_graph(self):
         # Create some sample transactions
-        user = User.objects.create_user(username='testuser', password='testpass')
+        self.client.login(username=self.user.username, password='Password123')
         transaction1 = Transaction.objects.create(user=user, transaction_type='Income', amount=100, date_paid=datetime.date(2022, 1, 1))
         transaction2 = Transaction.objects.create(user=user, transaction_type='Expense', amount=50, date_paid=datetime.date(2022, 1, 2))
         transaction3 = Transaction.objects.create(user=user, transaction_type='Expense', amount=25, date_paid=datetime.date(2022, 1, 3))
@@ -96,9 +81,9 @@ class DashboardTestCase(TestCase):
         self.assertEqual(data['labels'], ['01/01/2022', '01/02/2022', '01/03/2022'])
         self.assertEqual(data['data'], [100, 0, 0])
     
-    def test_expense_structure_with_transactions(self):
+    #def test_expense_structure_with_transactions(self):
         # Create test user
-        user = User.objects.create_user(username='testuser', password='testpass')
+        self.client.login(username=self.user.username, password='Password123')
         # Create test transactions
         Transaction.objects.create(user=user, transaction_type='Expense', category='Food', amount=10.0)
         Transaction.objects.create(user=user, transaction_type='Expense', category='Food', amount=20.0)
@@ -114,7 +99,7 @@ class DashboardTestCase(TestCase):
         self.assertContains(response, '<td>Rent</td><td>$100.00</td>', html=True)
 
 
-    def test_expense_structure_with_no_transactions():
+    #def test_expense_structure_with_no_transactions():
         expense_structure = ExpenseStructure('Groceries')
         assert expense_structure.name == 'Groceries'
         assert expense_structure.total() == 0
