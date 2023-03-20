f45a4d44b3d1779e23d1a00018a7787fe4f9c0ac
Janet <k21003182@kcl.ac.uk>
2023-03-20 20:24:15 +0000

    Remove redundant code

--- spendingtrackers/helpers.py
+++ spendingtrackers/helpers.py
@@ -12,31 +12,6 @@ def get_user_transactions(user):
     transactions = Transaction.objects.filter(user=user)
     return transactions
 
-def get_user_income(user,from_date=None, to_date=None):
-    transactions = Transaction.objects.filter(user=user)
-    if from_date and to_date:
-        transactions = transactions.filter(date_paid__range=[from_date, to_date])
-    return sum(transaction.amount for transaction in transactions if transaction.transaction_type == 'Income')
-
-def get_user_expense(user,from_date=None, to_date=None):
-    transactions = Transaction.objects.filter(user=user)
-    if from_date and to_date:
-        transactions = transactions.filter(date_paid__range=[from_date, to_date])
-    return sum(transaction.amount for transaction in transactions if transaction.transaction_type == 'Expense')
-
-def get_user_budget(user,from_date=None, to_date=None):
-    categories = Category.objects.filter(user=user)
-    overall_budget = 0
-    for category in categories:
-        overall_budget= overall_budget+category.budget
-    return overall_budget
-
-def get_user_balance(user,from_date=None, to_date=None):
-    if(from_date == None or to_date ==  None):
-        balance =  get_user_budget(user,None,None)- get_user_expense(user,None,None)
-    else:
-        balance = get_user_budget(user,from_date,to_date) - get_user_expense(user,from_date,to_date)
-    return balance
 
 def change_transaction_name(user,category):
     transactions = Transaction.objects.filter(user=user)
@@ -83,20 +58,7 @@ def get_user_balance(user, from_date=None, to_date=None):
     return balance
 
 
-def change_transaction_name(user, category):
-    transactions = Transaction.objects.filter(user=user)
-    new_name = category.name
-    for transaction in transactions:
-        if transaction.category_fk == category:
-            transaction.category = new_name
-            transaction.save()
 
-
-def delete_transactions(user, category):
-    transactions = Transaction.objects.filter(user=user)
-    for transaction in transactions:
-        if transaction.category_fk == category:
-            transaction.delete()
 
 
 
