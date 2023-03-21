e1fd84b2092cdc29219dcfe6beed21806790418a
Aayush Dwivedi <K21077065@kcl.ac.uk>
2023-03-21 12:01:14 +0000

    clean code adjustments to the category views

--- spendingtrackers/views/category_views/__init__.py
+++ spendingtrackers/views/category_views/__init__.py
@@ -1,14 +1,4 @@
 # Create your views here.
-from .delete_lesson_request import LessonRequestDeleteView
-from .home import home
-from .log_in import log_in
-from .log_out import log_out
-from .sign_up import sign_up
-from .update_lesson_request import LessonRequestUpdateView
-from .view_lesson_request import view_lesson_request
-
-import re
-from django.shortcuts import render, redirect, get_object_or_404
 from ..models import User, Transaction, Category, Achievement, Chart
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
